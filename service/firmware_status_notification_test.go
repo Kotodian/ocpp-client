70f28807ff4197e04262e94cec9ea4302f67564d
linqiankai <417350372@qq.com>
2021-05-25 11:12:35 +0800

    命令完善

--- service/firmware_status_notification_test.go
+++ service/firmware_status_notification_test.go
@@ -1,1 +1,19 @@
 package service
+
+import (
+	"github.com/stretchr/testify/assert"
+	"ocpp-client/message"
+	"testing"
+)
+
+func TestChargeStation_FirmwareStatusNotificationRequestResponse(t *testing.T) {
+	chargeStation := NewChargeStation("T164713520")
+
+	msg, err := chargeStation.Function("2", "", "FirmwareStatusNotification")
+	assert.Nil(t, err)
+	assert.NotNil(t, msg)
+
+	_, id, _, payload := message.Parse(msg)
+	_, err = chargeStation.Function("3", id, "FirmwareStatusNotification", payload)
+	assert.Nil(t, err)
+}
