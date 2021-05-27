0bfb432d55c25adbf4b0e49dae94eacaa4b9fef3
linqiankai <417350372@qq.com>
2021-05-27 09:02:39 +0800

    TransactionEvent

--- service/transaction_event.go
+++ service/transaction_event.go
@@ -1,6 +1,42 @@
 package service
 
-func (c *ChargeStation) TransactionEvent() ([]byte, error) {
+import (
+	"encoding/json"
+	"ocpp-client/message"
+	"time"
+)
 
-	return nil, nil
+func (c *ChargeStation) TransactionEventRequest() ([]byte, error) {
+	request := &message.TransactionEventRequestJson{
+		EventType: message.TransactionEventEnumType_1_Ended,
+		MeterValue: []message.MeterValueType_1{
+			message.MeterValueType_1{
+				SampledValue: []message.SampledValueType_1{
+					message.SampledValueType_1{
+						Context: nil,
+					},
+				},
+				Timestamp: time.Now().Format(time.RFC3339),
+			},
+			message.MeterValueType_1{
+				SampledValue: []message.SampledValueType_1{
+					message.SampledValueType_1{
+						Context: nil,
+					},
+				},
+				Timestamp: time.Now().Format(time.RFC3339),
+			},
+		},
+		Timestamp: time.Now().Format(time.RFC3339),
+		TransactionInfo: message.TransactionType{
+			TransactionId: c.transaction.id,
+		},
+	}
+	msg, _, err := message.New("2", "TransactionEvent", request)
+	return msg, err
+}
+
+func (c *ChargeStation) TransactionEventResponse(msgID string, msg []byte) error {
+	response := &message.TransactionEventResponseJson{}
+	return json.Unmarshal(msg, response)
 }
