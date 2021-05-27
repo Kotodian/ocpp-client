935412005b326b5270d9241f79144780b97855fa
林å千凯 <417350372@qq.com>
2021-05-27 09:27:35 +0800

    测试生成的sn是不是独立的

--- api/charge_station_test.go
+++ api/charge_station_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -10,4 +11,11 @@ func TestFormatSN(t *testing.T) {
 	assert.Equal(t, "T1641735000", formatSN(sn))
 	sn = "T164173500000000000"
 	assert.Equal(t, "T1641735000", formatSN(sn))
+}
+
+func TestCreateMultipleChargeStation(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		sn := "T16417352" + fmt.Sprint(i)
+		fmt.Println(formatSN(sn))
+	}
 }
