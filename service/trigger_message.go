d8db4b4b82f06438dc57c84a75e5df71e3232241
linqiankai <417350372@qq.com>
2021-06-07 13:39:05 +0800

    支持TriggerMessage

--- service/trigger_message.go
+++ service/trigger_message.go
@@ -1,1 +1,19 @@
 package service
+
+import (
+	"encoding/json"
+	"ocpp-client/message"
+)
+
+func (c *ChargeStation) TriggerMessageResponse(msgID string, msg []byte) ([]byte, error) {
+	request := &message.TriggerMessageRequestJson{}
+	err := json.Unmarshal(msg, request)
+	if err != nil {
+		return nil, err
+	}
+	response := &message.TriggerMessageResponseJson{
+		Status: message.TriggerMessageStatusEnumType_1_Accepted,
+	}
+	msg, _, err = message.New("3", "TriggerMessage", response, msgID)
+	return msg, err
+}
